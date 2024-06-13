e5f432d6ae85e61d5ac9e561651543fe81d46d17
gsoaresslv <guilhermesoaresvixbr@gmail.com>
2024-06-13 08:43:23 -0300

    Changes to be committed: 	modified:   css/styles.css 	modified:   index.html 	modified:   js/avalia.js 	modified:   js/cadastro.js 	deleted:    js/index.js 	modified:   js/login.js

--- js/avalia.js
+++ js/avalia.js
@@ -1,24 +1,29 @@
-const newfeedback= document.getElementById("new-feedback");
-const btnnewfeedback= document.getElementById("btn-new-feedback");
+//  recebe as informações do formulário de avaliação e exibe na tela
+const newfeedback= document.getElementById("new-feedback");// Formulário de avaliação
+const btnnewfeedback= document.getElementById("btn-new-feedback"); // Botão de avaliação
 
-function displayElement(elementId) {
-    var id = document.getElementById(elementId);
-    id.style.display = "flex";
+// Evento de clique no botão de avaliação
+function displayElement(elementId) { // Função para exibir um elemento
+    var id = document.getElementById(elementId); // Pega o elemento pelo ID
+    id.style.display = "flex"; // Exibe o elemento
 }
-function hideElement(elementId) {
-    document.getElementById(elementId).style.display = "none";
-}
-function enviaAval(){
-    const nome = document.getElementById("inputNome").value;
-    const resenha = document.getElementById("inputResenha").value;
 
-    if(nome == "" && resenha ==""){
-        document.getElementById("erroFeedback").innerHTML = "Preencha os campos antes de enviar!"
+// Evento de clique no botão de avaliação
+function hideElement(elementId) { // Função para esconder um elemento
+    document.getElementById(elementId).style.display = "none"; // Esconde o elemento
+}
+// Evento de clique no botão de avaliação
+function enviaAval(){ // Função para enviar a avaliação
+    const nome = document.getElementById("inputNome").value; // Pega o valor do campo nome
+    const resenha = document.getElementById("inputResenha").value; // Pega o valor do campo resenha
+
+    if(nome == "" && resenha ==""){ // Se os campos estiverem vazios
+        document.getElementById("erroFeedback").innerHTML = "Preencha os campos antes de enviar!" // Exibe a mensagem de erro
     }
-        else{
-            document.getElementById("nome-feedback").innerHTML = nome;
-            document.getElementById("resenha-feedback").innerHTML = resenha;
-            hideElement("new-feedback");
-            displayElement("your-feedback");
+        else{ // Se os campos estiverem preenchidos
+            document.getElementById("nome-feedback").innerHTML = nome; // Exibe o nome
+            document.getElementById("resenha-feedback").innerHTML = resenha; // Exibe a resenha
+            hideElement("new-feedback"); // Esconde o formulário de avaliação
+            displayElement("your-feedback"); // Exibe a avaliação
         }
 }
