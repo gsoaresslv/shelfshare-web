ec5fc411f46340c66a9401aeae9995635ff461f3
Maria Lira <maria@gmail.com>
2024-10-24 11:07:54 -0300

    perfil 	modified:   css/perfil.css 	new file:   js/perfil.js 	modified:   perfil.html

---
+++ js/perfil.js
@@ -0,0 +1,8 @@
+// Funções básicas para os botões
+document.getElementById("addBookBtn").addEventListener("click", function() {
+    alert("Adicionar livro clicado!");
+});
+
+document.getElementById("editProfileBtn").addEventListener("click", function() {
+    alert("Editar perfil clicado!");
+});
