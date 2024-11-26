1be2a5a4227164dfbfe9b333ae0340a38d45ec4d
Guilherme Silva <guilhermesoaresvixbr@gmail.com>
2024-11-26 17:45:48 -0300

    coment navbar 	modified:   navbar.php

--- navbar.php
+++ navbar.php
@@ -8,11 +8,11 @@
 <body>
 <header>
     <nav>
-        <div class="navbar">
+        <div class="navbar"><!-- Container principal da barra de navegação -->
             <a href="inicial.php">
                 <h2>Shelfshare </h2>
             </a>
-            <div class="buscar-box">
+            <div class="buscar-box"><!-- Caixa de busca na barra de navegação -->
                 <div class="input-buscar">
                     <input type="text" placeholder="Faça uma busca">
                 </div>
@@ -22,7 +22,7 @@
                     </svg>    
                 </div>
             </div>
-            <div class="icones">
+            <div class="icones"><!-- Ícones de navegação, representando diferentes ações -->
                 <a href="cadastrarlivros.php">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
                         <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
